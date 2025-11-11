4897fdb6492080a2c6876301b66ea681a6571aa6
duckennguyen <198175894+duckennguyen@users.noreply.github.com>
2025-11-11 14:32:54 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,32 @@
 local function sanitizeClipboard() 
     local img = hs.pasteboard.readImage() 
-    if string.sub(app::bundleID(), 1, #"com.microsoft") ~= "com.microsoft" then
+    local txt = hs.pasteboard.readString()
+    local url = hs.pasteboard.readURL()
+    local app = hs.application.frontmostApplication()
+    local log = hs.logger.new('sanitizer','debug')
+    if string.sub(app:bundleID(), 1, #"com.microsoft") ~= "com.microsoft" then
         if img ~= nil then 
             clipboardWatcher:stop() 
             hs.pasteboard.clearContents() 
             hs.pasteboard.writeObjects(img) 
             clipboardWatcher:start()
-            --local log = hs.logger.new('sanitizer','debug')
-            --log.i('Stripping URL from copied image')
+            log.i('Stripping URL from copied image')
         end
-    end 
+    else
+        if img ~= nil and url~=nil then 
+            clipboardWatcher:stop() 
+            hs.pasteboard.clearContents() 
+            hs.pasteboard.writeObjects(img) 
+            clipboardWatcher:start()
+            log.i('Word: Stripping URL from copied image')
+        elseif txt ~= nil then
+            clipboardWatcher:stop() 
+            hs.pasteboard.clearContents() 
+            hs.pasteboard.writeObjects(txt) 
+            clipboardWatcher:start()
+            log.i('Word: Stripping image from copied text')
+        end
+    end
 end
 
 clipboardWatcher = hs.pasteboard.watcher.new(sanitizeClipboard) 
